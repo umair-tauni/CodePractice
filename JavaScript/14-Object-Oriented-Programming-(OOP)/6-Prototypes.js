8ceee5d733e14a6c367de09e70d1b38e471594ed
Umair Tauni <engrumayr@gmail.com>
2024-03-04 16:54:14 +0500

    first commit

---
+++ JavaScript/14-Object-Oriented-Programming-(OOP)/6-Prototypes.js
@@ -0,0 +1,54 @@
+"use strict";
+
+const Person = function (firstName, birthYear) {
+  //   console.log(this);
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  //   Never do this
+  //   this.calcAge = function () {
+  // console.log(2037 - this.birthYear);
+  //   };
+};
+
+const jonas = new Person("Jonas", 1991);
+console.log(jonas);
+
+// 1. New {} is created
+// 2. function is called, this {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
+
+const matilda = new Person("Matilda", 2017);
+const jack = new Person("Jack", 1975);
+console.log(matilda, jack);
+
+console.log(jonas instanceof Person);
+
+// Prototypes
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
+matilda.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(jack));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+// .prototypeOfLinkedObjects
+
+Person.prototype.species = "Homo Sapiens";
+console.log(jonas, matilda);
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));
+
