5bc316d5c7ab068dea941aa7c07c179e971e4892
Umair Tauni <engrumayr@gmail.com>
2024-03-13 15:22:53 +0500

    first commit

---
+++ JavaScript/JavaScript-Jonas/16-Asynchronous-JS-Promises-Async-Await-AJAX/9-Consuming-Promises.js
@@ -0,0 +1,46 @@
+"use strict";
+
+const btn = document.querySelector(".btn-country");
+const countriesContainer = document.querySelector(".countries");
+
+const renderCountry = function (data, className = "") {
+  const html = `<article class="country ${className}">
+      <img class="country__img" src="${data.flags.svg}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name.common}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)} people</p>
+        <p class="country__row"><span>🗣️</span>${
+          data?.languages[Object.keys(data?.languages)[0]]
+        }</p>
+        <p class="country__row"><span>💰</span>${
+          data?.currencies[Object.keys(data?.currencies)[0]]?.name
+        }</p>
+      </div>
+    </article>`;
+
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
+};
+
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v3.1/name/${country}`)
+//     .then(function (response) {
+//       console.log(response);
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
+// };
+
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then((response) => response.json())
+    .then((data) => renderCountry(data[0]));
+};
+
+getCountryData("portugal");
