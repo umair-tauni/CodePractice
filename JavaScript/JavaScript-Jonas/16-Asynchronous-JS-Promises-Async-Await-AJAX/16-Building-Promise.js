ac629dc079d3ed6460fa4bb86a2974fbaa286ec5
Umair Tauni <engrumayr@gmail.com>
2024-03-15 16:29:20 +0500

    first commit

--- JavaScript/JavaScript-Jonas/16-Asynchronous-JS-Promises-Async-Await-AJAX/16-Building-Promise.js
+++ JavaScript/JavaScript-Jonas/16-Asynchronous-JS-Promises-Async-Await-AJAX/16-Building-Promise.js
@@ -14,3 +14,31 @@ const lotteryPromise = new Promise(function (resolve, reject) {
 lotteryPromise
   .then((res) => console.log(res))
   .catch((err) => console.error(err));
+
+// Promisifying
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+wait(1)
+  .then(() => {
+    console.log("1 second passed");
+    return wait(1);
+  })
+  .then(() => {
+    console.log("2 second passed");
+    return wait(1);
+  })
+  .then(() => {
+    console.log("3 second passed");
+    return wait(1);
+  })
+  .then(() => {
+    console.log("4 second passed");
+  });
+
+// static method on Promise constructor
+Promise.resolve("abc").then((res) => console.log(res));
+Promise.reject(new Error("Problem!")).catch((err) => console.error(err));
